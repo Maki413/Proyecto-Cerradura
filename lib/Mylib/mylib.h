9a25ea747bf363fc93caa5873f11544ff057241a
Luis <marioaz.01042003@gmail.com>
2024-08-15 01:02:29 -0500

    add Mapbits to Mylib

--- lib/Mylib/mylib.h
+++ lib/Mylib/mylib.h
@@ -1,5 +1,10 @@
 #ifndef MYLIB_H  // Si MYLIB_H no está definido
 #define MYLIB_H  // Define MYLIB_H
+#include <stdint.h>
+#include <Arduino.h>
+
+extern uint8_t interfazCerrada PROGMEM[];
+extern uint8_t logoFausti PROGMEM[];
 void encender_rojo();
 void encender_verde();
 #endif
